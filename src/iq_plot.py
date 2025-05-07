cd0a4a3b6caf9a339c5efd8019d7c04590dabe7b
Richard Beattie <richard.beattie91@gmail.com>
2025-05-07 11:47:39 -0400

    Add heatmap plot

--- src/iq_plot.py
+++ src/iq_plot.py
@@ -46,3 +46,39 @@ class VibrationIntensityPlot(QtWidgets.QMainWindow):
         if not points:
             return
         self.line.setData(*zip(*points))
+
+
+class VibrationIntensityHeatmap(QtWidgets.QMainWindow):
+    def __init__(self, start_freq: float):
+        super().__init__()
+
+        self.start_freq = start_freq
+
+        self.setWindowTitle("Vibration Intensity Heatmap")
+
+        self.plot_widget = pg.PlotWidget()
+        self.setCentralWidget(self.plot_widget)
+
+        self.img_item = pg.ImageItem()
+        self.plot_widget.addItem(self.img_item)
+        self.plot_widget.setLabel("bottom", "Frequency (Hz)")
+        self.plot_widget.setLabel("left", "Time Bins")
+        self.plot_widget.setAspectLocked(False)
+
+        # Optional: color map
+        cbi = pg.ColorBarItem(colorMap="CET-L3")
+        plot_item = self.plot_widget.getPlotItem()
+        cbi.setImageItem(self.img_item, insert_in=plot_item)
+
+    def update(self, data: np.ndarray):
+        if data.ndim != 2:
+            raise ValueError("Expected 2D array (n_bins, n_freqs)")
+
+        n_freqs, n_time_bins = data.shape
+
+        self.plot_widget.setXRange(self.start_freq, self.start_freq + n_freqs)
+
+        self.img_item.setImage(data, levels=(0, 4))
+
+        image_rect = QtCore.QRectF(self.start_freq, 0, n_freqs, n_time_bins)
+        self.img_item.setRect(image_rect)
