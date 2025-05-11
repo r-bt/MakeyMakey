cd8e8909531884070f7b8f4f8f416c55cc93bdc8
Richard Beattie <richard.beattie91@gmail.com>
2025-05-11 09:40:17 -0400

    Update record

--- record.py
+++ record.py
@@ -7,34 +7,16 @@ import os
 
 from src.radar import Radar
 
-log_queue = queue.Queue()
-stop_event = threading.Event()
-
 buffer = []
-
-
-def writer_thread(filename):
-    while not stop_event.is_set() or not log_queue.empty():
-        try:
-            item = log_queue.get(timeout=0.05)
-            buffer.append((item["timestamp"], item["data"]))
-        except queue.Empty:
-            continue
-
-    flush(buffer, filename)
-
-
-def flush(buffer, filename):
-    timestamps, data = zip(*buffer)
-    np.savez_compressed(filename, data=np.stack(data), timestamp=np.array(timestamps))
 
 
 def log(msg):
     """
     Callback function to log the data to the csv file
     """
+    global buffer
 
-    log_queue.put(msg)
+    buffer.append(msg["data"])
 
 
 if __name__ == "__main__":
@@ -47,12 +29,13 @@ if __name__ == "__main__":
     filename = f"data/radar_data_{datetime.now().strftime('%Y%m%d_%H%M%S')}.npz"
     os.makedirs("data", exist_ok=True)
 
-    thread = threading.Thread(target=writer_thread, args=(filename,))
-    thread.start()
-
     # Initialize the radar
     radar = Radar(args.cfg)
     radar.run_polling(cb=log)
 
-    stop_event.set()
-    thread.join()
+    print("Saving data...")
+
+    # Save the data
+    np.savez_compressed(filename, data=np.stack(buffer))
+
+    print(f"Saved data to {filename}")
